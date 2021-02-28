3cd16751b241464e0decef49f6235ba52a8556f4
Nikitonu4 <nikitonu4@gmail.com>
2021-02-28 20:37:43 +0300

    edited sosiski

--- sosiski.py
+++ sosiski.py
@@ -1,6 +1,11 @@
-k, n = map(int,input().split())
+# k, n = map(int,input().split())
 a = list()
-for i in range(k):
-         a.append(list(map(int, input().split())))
-
-print(a)
+# for i in range(k):
+#          a.append(list(map(int, input().split())))
+a = [[0,5], [13,18], [25,475]]
+sum = 0
+for i in range(len(a)):
+    for j in range(len(a[i])-1):
+        sum += ((a[i][j+1]-a[i][j])+1)
+        print(sum)
+print(sum)
